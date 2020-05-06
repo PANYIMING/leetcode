ddc80a42cf68d212edfed3d26a17b6056f4864c1
panyiming10776@hellobike.com <panyiming10776@hellobike.com>
2020-05-06 16:53:33 +0800

    [add] queue code

---
+++ data_structure/Queue.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class MyQueue {
+private:
+    // store elements
+    vector<int> data;
+    // a pointer to indicate the start position
+    int p_start;
+public:
+    MyQueue(){p_start = 0;}
+    /** insert an element into the queue, return true if the operation is successful */
+        bool enQueue(int x) {
+            data.push_back(x);
+            return true;
+        };
+    /** delete an element from the queue. return true if the operation is successful */
+        bool deQueue() {
+            if (isEmpty()){
+                return false;
+            }
+            p_start++;
+            return true;
+        };
+        /** get the front item from queue */
+        int Front() {
+            return data[p_start];
+        };
+        /** checks whether the queue is empty or not */
+        bool isEmpty(){
+            return p_start >= data.size();
+        }
+};
+
+int main()
+{
+    MyQueue q;
+    q.enQueue(5);
+    q.enQueue(3);
+    if (!q.isEmpty()){
+        cout << q.Front() << endl;
+    }
+    q.deQueue();
+    if (!q.isEmpty()){
+        cout << q.Front() << endl;
+    }
+    q.deQueue();
+    if (!q.isEmpty()){
+        cout << q.Front() << endl;
+    }
+
+}
